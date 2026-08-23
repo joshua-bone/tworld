56054edd6dce6b455d4801fda84ccfd8b1b48de6
Joshua Bone <joshua.bone@hotmail.com>
2026-08-23 16:19:57 -0700

    Fix HybridCC WASM asset closure

--- web/src/player-web/impl/hybridcc-v0/loadWasm.ts
+++ web/src/player-web/impl/hybridcc-v0/loadWasm.ts
@@ -1,20 +1,14 @@
-import engineJsUrl from "./engine/hybridcc_v0_wasm.js?url";
-import engineWasmUrl from "./engine/hybridcc_v0_wasm.wasm?url";
+import createHybridCcModule from "./engine/hybridcc_v0_wasm.js";
 import type { HybridCcWasmModule } from "./wasmBridge";
 
 let modulePromise: Promise<HybridCcWasmModule> | null = null;
-
-type HybridCcModuleFactory = (options: {
-  locateFile: () => string;
-}) => Promise<HybridCcWasmModule>;
+const engineWasmUrl = new URL("./engine/hybridcc_v0_wasm.wasm", import.meta.url).href;
 
 export function loadHybridCcWasm(): Promise<HybridCcWasmModule> {
   if (modulePromise) {
     return modulePromise;
   }
 
-  modulePromise = import(/* @vite-ignore */ engineJsUrl).then(({ default: factory }) =>
-    (factory as HybridCcModuleFactory)({ locateFile: () => engineWasmUrl }),
-  );
+  modulePromise = createHybridCcModule({ locateFile: () => engineWasmUrl });
   return modulePromise;
 }
